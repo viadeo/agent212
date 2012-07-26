3b6d218164d46fa5a1d0cdcc0c88c90699fb3c28
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2012-07-26 16:49:58 +0200

    it makes sense that the constructor accepts a ua string

--- lib/agent212.rb
+++ lib/agent212.rb
@@ -5,12 +5,15 @@ module Agent212
 
   class UserAgent
 
-    def initialize()
+    def initialize(user_agent_string)
       @parts = [] # either products or strings (comments)
+      Parser.parse(user_agent_string).each do |x|
+        @parts << x
+      end
     end
 
     def self.parse(user_agent_string)
-      Parser.parse(user_agent_string)
+      new user_agent_string
     end
 
     def products
