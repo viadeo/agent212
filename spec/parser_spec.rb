b109e7aae1d7ad9ec88321069197240218dee268
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2012-07-26 15:38:59 +0200

    tests

---
+++ spec/parser_spec.rb
@@ -0,0 +1,51 @@
+require 'agent212'
+
+describe Agent212::Parser do
+
+  context 'parsing tokens' do
+    it 'should parse a valid token' do
+      Agent212::Parser.new('foo').parse_token.should == 'foo'
+      Agent212::Parser.new('BaR').parse_token.should == 'BaR'
+      Agent212::Parser.new('23489.quux').parse_token.should == '23489.quux'
+      Agent212::Parser.new('URL%20encoded').parse_token.should == 'URL%20encoded'
+    end
+
+    it 'should not parse an invalid token' do
+      Agent212::Parser.new('(foo)').parse_token.should be_nil
+      Agent212::Parser.new('\tBaR').parse_token.should be_nil
+    end
+
+  end
+
+  context "parsing LWS" do
+    it 'should parse a space as whitespace' do
+      x = Agent212::Parser.new("foo foo")
+      x.parse_token
+      x.parse_lws.should == ' '
+    end
+
+    it 'should parse a tab as whitespace' do
+      x = Agent212::Parser.new("foo\tfoo")
+      x.parse_token
+      x.parse_lws.should == "\t"
+    end
+
+
+    it 'should not parse non-LWS' do
+      Agent212::Parser.new('foo').parse_lws.should be_nil
+      Agent212::Parser.new('bar ').parse_lws.should be_nil
+    end
+  end
+
+
+  context "parsing a whole user_agent string" do
+    it "" do
+      parser = Agent212::Parser.new("Viadeo%20Sync%20for%20the%20Mac/0.1.2 CFNetwork/520.4.3 Darwin/11.4.0 (x86_64) (MacBook6%2C1)")
+      x = parser.parse
+      x.should_not be_nil
+      puts x.inspect
+    end
+  end
+
+end
+
